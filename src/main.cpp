f909b5507155c2b92db3ae2fb8a468bbbf3db23d
Bumrang <maximkuznet@gmail.com>
2016-10-19 00:59:39 -0700

    First commit, only contains CMake build for Windows.

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800,600), "kulak");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(sf::Color (100,149,237,255));
+		window.display();
+	}
+
+	return 0;
+}
